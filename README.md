7086b19372d15014374b8d13f26fa1c2e8a9de0f
Lachlan Miller <lachlan.miller.1990@outlook.com>
2024-03-14 22:14:20 +1000

    docs

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 I am learning bioinformatics, this is the code I am writing.
+
+# Requirements
+
+- Python 3
+
+# Install
+
+```sh
+pip install -r requirements.txt
+```
+
+# Lint
+
+```sh
+black *.py
+```
+
+## Algorithms
+
+- [Needleman Wunsch](./needleman_wunsch.py)
+
+```sh
+# Basic
+python needleman_wunsch.py "GTCGACGCA" "GGATACA"
+
+# Custom weights
+python needleman_wunsch.py "GTCGACGCA" "GGATACA" --gap -1 --mismatch 0 --match 3
+```
--- needleman_wunsch.py
+++ needleman_wunsch.py
@@ -1,107 +1,111 @@
 import pandas as pd
 import numpy as np
 from typing import List, Dict, TypeAlias
+import argparse
 
-match = 1
-miss = -1
-gap = -2
+TracebackMap: TypeAlias = Dict[str, List[int]]
+Traceback: TypeAlias = List[List[int]]
 
-TracebackMap: TypeAlias = Dict[str,List[int]] 
+parser = argparse.ArgumentParser(description='Run Needleman Wunsch')
 
-def main():
-  # Define your sequences
-  s1 = "GTCGACGCA"
-  s2 = "GATTACA"
+parser.add_argument('s1', type=str, help='First sequence')
+parser.add_argument('s2', type=str, help='Second sequence')
 
-  # s1 = "GAA"
-  # s2 = "GGAA"
+parser.add_argument('--gap', type=int, default=-2, help='Gap penalty (default: -2)')
+parser.add_argument('--mismatch', type=int, default=-1, help='Mismatch penalty (default: -1)')
+parser.add_argument('--match', type=int, default=1, help='Match score (default: 1)')
 
-  # Initialize the extended matrix with zeros, accounting for the extra row and column
-  m = np.zeros((len(s1) + 1, len(s2) + 1), dtype=int)
+# Parse the arguments
+args = parser.parse_args()
 
-  # Convert the NumPy array to a Pandas DataFrame with placeholders for the extra row and column
-  df = pd.DataFrame(m, index=[''] + list(s1), columns=[''] + list(s2))
+gap = args.gap
+mismatch = args.mismatch
+match = args.match
 
-  t = np.empty((len(s1) + 1, len(s2) + 1), dtype=object)
-  traceback = pd.DataFrame(t, index=[''] + list(s1), columns=[''] + list(s2))
+def align(df: pd.DataFrame, trace: Traceback) -> tuple[str, str]:
+    a1 = ""
+    a2 = ""
+    previ = 0
+    prevj = 0
 
-  # Update the first row and first column with the decrementing values
-  df.iloc[0, :] = np.arange(0, -2 * (len(s2) + 1), -2)  # First row
-  df.iloc[:, 0] = np.arange(0, -2 * (len(s1) + 1), -2)  # First column
+    for [i, j] in trace:
+        if i != previ:
+            a1 = a1 + df.index[i]
+        else:
+            # horizontal
+            a1 = a1 + "-"
+        if j != prevj:
+            a2 = a2 + df.columns[j]
+        else:
+            # vertical
+            a2 = a2 + "-"
+        previ = i
+        prevj = j
+    return a1, a2
 
-  def make_traceback(df: pd.DataFrame, traceback_map: TracebackMap) -> List[List[int]]:
+
+def make_traceback(df: pd.DataFrame, traceback_map: TracebackMap) -> Traceback:
     [i, j] = df.shape
-    i = i-1
-    j = j-1
-    path: List[List[int]] = [[i,j]]
-    next = traceback_map[f"{i},{j}"]
+    i = i - 1
+    j = j - 1
+    path: List[List[int]] = [[i, j]]
+    nxt = traceback_map[f"{i},{j}"]
 
-    while next != [0,0]:
-      path.append(next)
-      i = next[0]
-      j = next[1]
-      next = traceback_map[f"{i},{j}"]
+    while nxt != [0, 0]:
+        path.append(nxt)
+        i = nxt[0]
+        j = nxt[1]
+        nxt = traceback_map[f"{i},{j}"]
 
     path.reverse()
     return path
 
-  traceback_map: TracebackMap = {}
 
-  for i in range(df.shape[0]):  # Iterate over rows
-    for j in range(df.shape[1]):  # Iterate over columns
-        value = df.iat[i, j]
-        if i == 0 and j == 0:
-          pass
-        elif value == 0:
-          up = df.iat[i-1, j] + gap
-          left = df.iat[i ,j-1] + gap
-          ismatch = match if df.index[i] == df.columns[j] else miss
-          diag = ismatch + df.iat[i-1,j-1]
-          best = max(diag, up, left)
-          df.iat[i, j] = best
-          def fn(dir): 
-            pass
-            # print(f"best for [{i},{j}] ({df.index[i]} {df.columns[j]}) is {dir} ", end="")
+def main(s1, s2) -> tuple[str, str]:
+    # Define your sequences
+    s1 = "GTCGACGCA"
+    s2 = "GATTACA"
 
-          if best == up:
-            fn("up")
-            # print(f"from [{i-1}, {j}]")
-            traceback_map[f"{i},{j}"] = [i-1,j]
-            traceback.iloc[i,j] = [i-1,j]
-          elif best == left:
-            fn("left")
-            # print(f"from [{i}, {j-1}]")
-            traceback_map[f"{i},{j}"] = [i,j-1]
-            traceback.iat[i, j] = [i,j-1]
-          elif best == diag:
-            fn("diag")
-            # print(f"from [{i-1}, {j-1}]")
-            traceback_map[f"{i},{j}"] = [i-1,j-1]
-            traceback.iat[i, j] = [i-1,j-1]
+    # Initialize the extended matrix with zeros, accounting for the extra row and column
+    m = np.zeros((len(s1) + 1, len(s2) + 1), dtype=int)
 
-  trace = make_traceback(df, traceback_map)
-  a1 = ""
-  a2 = ""
-  previ = 0
-  prevj = 0
+    # Convert the NumPy array to a Pandas DataFrame with placeholders for the extra row and column
+    df = pd.DataFrame(m, index=[""] + list(s1), columns=[""] + list(s2))
 
-  for [i,j] in trace:
-    if i != previ:
-      a1 = a1 + df.index[i]
-    else:
-      # horizontal
-      a1 = a1 + "-"
-    if j != prevj:
-      a2 = a2 + df.columns[j]
-    else:
-      # vertical
-      a2 = a2 + "-"
-    previ = i
-    prevj = j
+    t = np.empty((len(s1) + 1, len(s2) + 1), dtype=object)
 
-  return df, a1, a2
+    # Update the first row and first column with the decrementing values
+    df.iloc[0, :] = np.arange(0, -2 * (len(s2) + 1), -2)  # First row
+    df.iloc[:, 0] = np.arange(0, -2 * (len(s1) + 1), -2)  # First column
 
-df, a1, a2 = main()
+    traceback_map: TracebackMap = {}
 
-print(a1)
-print(a2)
+    for i in range(df.shape[0]):
+        for j in range(df.shape[1]):
+            # Do not mutate gap values (first row/col)
+            if i == 0 or j == 0:
+                continue
+            # Otherwise, do the calc
+            up = df.iat[i - 1, j] + gap
+            left = df.iat[i, j - 1] + gap
+            ismatch = match if df.index[i] == df.columns[j] else mismatch
+            diag = ismatch + df.iat[i - 1, j - 1]
+            best = max(diag, up, left)
+            df.iat[i, j] = best
+
+            if best == up:
+                traceback_map[f"{i},{j}"] = [i - 1, j]
+            elif best == left:
+                traceback_map[f"{i},{j}"] = [i, j - 1]
+            elif best == diag:
+                traceback_map[f"{i},{j}"] = [i - 1, j - 1]
+
+    trace = make_traceback(df, traceback_map)
+    a1, a2 = align(df, trace)
+
+    return a1, a2
+
+if __name__ == '__main__':
+  s1, s2 = main(args.s1, args.s2)
+  print(s1)
+  print(s2)
